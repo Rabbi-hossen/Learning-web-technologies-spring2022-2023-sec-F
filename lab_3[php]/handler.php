978e6b8201cd2ff1d1866dec7897004c8a7e63c3
RABBI HOSSEN <73291723+Rabbi-hossen@users.noreply.github.com>
2023-02-20 02:48:57 +0600

    lab_3[php]

---
+++ lab_3[php]/handler.php
@@ -0,0 +1,40 @@
+<html>
+<?php
+
+ if(isset($_REQUEST['name'])) {
+    echo $_REQUEST['name'];
+    echo "<br>";
+} 
+
+if(isset($_REQUEST['email']) ){
+    echo $_REQUEST['email'];
+    echo "<br>";
+}
+
+
+
+if(isset($_REQUEST['date'])){
+    echo $_REQUEST['date'];
+    echo "<br>";
+}
+
+
+if(isset($_REQUEST['gender'])){
+    echo $_REQUEST['gender'];
+    echo "<br>";
+}
+if(isset($_REQUEST['degree'])){
+    echo $_REQUEST['degree'];
+    echo "<br>";
+}
+
+if(isset($_REQUEST['blood'])){
+    echo $_REQUEST['blood'];
+    echo "<br>";
+}
+
+
+
+
+?>
+</html>
