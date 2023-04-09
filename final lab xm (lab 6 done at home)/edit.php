a95b296e1c7ea6be39120905cb13746fca994e0c
RABBI HOSSEN <73291723+Rabbi-hossen@users.noreply.github.com>
2023-04-10 02:23:59 +0600

    done at home

---
+++ final lab xm (lab 6 done at home)/edit.php
@@ -0,0 +1,42 @@
+<form action="updated.php" method="post">
+    <fieldset>
+        <legend>Edit Product</legend>
+
+        <?php
+        $con = mysqli_connect('localhost', 'root', '', 'product_db');
+
+        // print_r($_REQUEST);
+       if(isset($_REQUEST['name'])){
+
+        $name = $_REQUEST['name'];
+        $dl = "SELECT * FROM `products` WHERE name = '{$name}' ";
+        $result = mysqli_query($con, $dl);
+        $row = mysqli_fetch_assoc($result);
+
+        //session_start();
+
+        //$_SESSION["name"] = $row['name'];
+
+        $name = $row['name'];
+        $bprice = $row['bprice'];
+        $sprice = $row['sprice'];
+       }
+        ?>
+
+
+                product name --- buying price ----- selling price <br>
+                <input type="text" name="name" value="<?php echo $name ?>">
+                <input type="number" name="bprice" value="<?php echo $bprice ?>">
+                <input type="number" name="sprice" value="<?php echo $sprice ?>">
+                <input type="submit" name="up" value="update">
+
+            </fieldset>
+            
+        </form>
+
+
+        
+
+
+    </fieldset>
+</form>
